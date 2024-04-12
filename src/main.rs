45419b35b46dca95b3da1185b342a6cfee1d5206
Sousa <68487926+Telmo-Sousa@users.noreply.github.com>
2024-04-12 17:10:06 +0100

    bullet limit and more

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use ggez::error::GameError;
 use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, Text};
-use ggez::{event::{self, EventHandler, KeyCode}, Context, GameResult};
+use ggez::{
+    event::{self, EventHandler, KeyCode},
+    Context, GameResult,
+};
 use rand::{thread_rng, Rng};
 use std::time::{Duration, Instant};
 
@@ -22,6 +25,8 @@ struct MainState {
     level: i32,
     player_lost: bool,
     menu_active: bool,
+    bullets_limit: i32,
+    bullets_on_screen: i32,
 }
 
 impl MainState {
@@ -35,14 +40,18 @@ impl MainState {
             level: 0,
             player_lost: false,
             menu_active: true,
+            bullets_limit: 0,
+            bullets_on_screen: 0,
         }
     }
 
     fn start_level(&mut self) {
         self.enemies.clear();
-        for _ in 0..(10 * self.level) {
+        let enemies_count = 10 * self.level;
+        for _ in 0..enemies_count {
             self.spawn_enemy();
         }
+        self.bullets_limit = enemies_count + 10;
     }
 
     fn spawn_enemy(&mut self) {
@@ -71,14 +80,29 @@ impl MainState {
     }
 
     fn shoot(&mut self) {
-        self.bullets.push((
-            self.player_x + PLAYER_SIZE / 2.0 - BULLET_SIZE / 2.0,
-            self.player_y + PLAYER_SIZE / 2.0 - BULLET_SIZE / 2.0,
-        ));
+        if self.bullets_limit > 0 && self.bullets_on_screen < 5 {
+            self.bullets.push((
+                self.player_x + PLAYER_SIZE / 2.0 - BULLET_SIZE / 2.0,
+                self.player_y + PLAYER_SIZE / 2.0 - BULLET_SIZE / 2.0,
+            ));
+            self.bullets_limit -= 1;
+            self.bullets_on_screen += 1;
+        }
     }
 
     fn update_bullets(&mut self) {
-        self.bullets.retain(|(x, _)| *x <= WINDOW_WIDTH);
+        let mut bullets_to_remove = Vec::new();
+        for (i, (x, _)) in self.bullets.iter().enumerate() {
+            if *x > WINDOW_WIDTH {
+                bullets_to_remove.push(i);
+            }
+        }
+        for idx in bullets_to_remove.into_iter().rev() {
+            if idx < self.bullets.len() {
+                self.bullets.remove(idx);
+                self.bullets_on_screen -= 1;
+            }
+        }
         for (x, y) in &mut self.bullets {
             *x += BULLET_SPEED;
         }
@@ -87,7 +111,7 @@ impl MainState {
     fn detect_collisions(&mut self) {
         let mut bullets_to_remove = Vec::new();
         let mut enemies_to_remove = Vec::new();
-    
+
         for (bullet_idx, bullet) in self.bullets.iter().enumerate() {
             let bullet_rect = graphics::Rect::new(bullet.0, bullet.1, BULLET_SIZE, BULLET_SIZE);
             for (enemy_idx, enemy) in self.enemies.iter().enumerate() {
@@ -98,7 +122,7 @@ impl MainState {
                 }
             }
         }
-    
+
         // Remove enemies first
         for idx in enemies_to_remove.into_iter().rev() {
             if idx < self.enemies.len() {
@@ -106,20 +130,21 @@ impl MainState {
                 self.score += 1;
             }
         }
-    
+
         // Remove bullets
         for idx in bullets_to_remove.into_iter().rev() {
             if idx < self.bullets.len() {
                 self.bullets.remove(idx);
+                self.bullets_on_screen -= 1;
             }
         }
     }
-    
 
     fn detect_player_enemy_collision(&self) -> bool {
         for enemy in &self.enemies {
             let enemy_rect = graphics::Rect::new(enemy.0, enemy.1, ENEMY_SIZE, ENEMY_SIZE);
-            let player_rect = graphics::Rect::new(self.player_x, self.player_y, PLAYER_SIZE, PLAYER_SIZE);
+            let player_rect =
+                graphics::Rect::new(self.player_x, self.player_y, PLAYER_SIZE, PLAYER_SIZE);
             if enemy_rect.overlaps(&player_rect) {
                 return true;
             }
@@ -157,21 +182,21 @@ impl EventHandler<GameError> for MainState {
 
         Ok(())
     }
-    
+
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         if self.menu_active {
             graphics::clear(ctx, Color::BLACK);
 
             let welcome_text = Text::new("game!");
             let welcome_position = [
-                WINDOW_WIDTH / 2.0 - welcome_text.width(ctx) as f32 / 2.0, 
+                WINDOW_WIDTH / 2.0 - welcome_text.width(ctx) as f32 / 2.0,
                 WINDOW_HEIGHT / 2.0 - 20.0,
             ];
             graphics::draw(ctx, &welcome_text, (welcome_position, 0.0, Color::WHITE))?;
 
             let start_text = Text::new("press space");
             let start_position = [
-                WINDOW_WIDTH / 2.0 - start_text.width(ctx) as f32 / 2.0, 
+                WINDOW_WIDTH / 2.0 - start_text.width(ctx) as f32 / 2.0,
                 WINDOW_HEIGHT / 2.0 + 20.0,
             ];
             graphics::draw(ctx, &start_text, (start_position, 0.0, Color::WHITE))?;
@@ -181,7 +206,7 @@ impl EventHandler<GameError> for MainState {
         }
 
         graphics::clear(ctx, Color::BLACK);
-    
+
         let player_mesh = Mesh::new_rectangle(
             ctx,
             DrawMode::fill(),
@@ -189,7 +214,7 @@ impl EventHandler<GameError> for MainState {
             Color::RED,
         )?;
         graphics::draw(ctx, &player_mesh, DrawParam::default())?;
-    
+
         for enemy in &self.enemies {
             let enemy_mesh = Mesh::new_rectangle(
                 ctx,
@@ -199,7 +224,7 @@ impl EventHandler<GameError> for MainState {
             )?;
             graphics::draw(ctx, &enemy_mesh, DrawParam::default())?;
         }
-    
+
         for (x, y) in &self.bullets {
             let bullet_mesh = Mesh::new_rectangle(
                 ctx,
@@ -209,25 +234,32 @@ impl EventHandler<GameError> for MainState {
             )?;
             graphics::draw(ctx, &bullet_mesh, DrawParam::default())?;
         }
-    
+
         let score_text = Text::new(format!("Score: {}", self.score));
         let score_position = [
-            WINDOW_WIDTH / 2.0 - score_text.width(ctx) as f32 / 2.0, 
-            10.0
+            WINDOW_WIDTH / 2.0 - score_text.width(ctx) as f32 / 2.0,
+            10.0,
         ];
         graphics::draw(ctx, &score_text, (score_position, 0.0, Color::WHITE))?;
-        
+
         let level_text = Text::new(format!("Level: {}", self.level));
         let level_position = [
-            WINDOW_WIDTH / 2.0 - level_text.width(ctx) as f32 / 2.0, 
-            26.0
+            WINDOW_WIDTH / 2.0 - level_text.width(ctx) as f32 / 2.0,
+            26.0,
         ];
         graphics::draw(ctx, &level_text, (level_position, 0.0, Color::WHITE))?;
-        
+
+        let bullets_text = Text::new(format!("Bullets: {}", self.bullets_limit));
+        let bullets_position = [
+            WINDOW_WIDTH - bullets_text.width(ctx) as f32 - 10.0,
+            WINDOW_HEIGHT - bullets_text.height(ctx) as f32 - 10.0,
+        ];
+        graphics::draw(ctx, &bullets_text, (bullets_position, 0.0, Color::WHITE))?;
+
         if self.player_lost {
             let lost_text = Text::new("you lost, press space");
             let lost_position = [
-                WINDOW_WIDTH / 2.0 - lost_text.width(ctx) as f32 / 2.0, 
+                WINDOW_WIDTH / 2.0 - lost_text.width(ctx) as f32 / 2.0,
                 WINDOW_HEIGHT / 2.0,
             ];
             graphics::draw(ctx, &lost_text, (lost_position, 0.0, Color::WHITE))?;
@@ -236,16 +268,16 @@ impl EventHandler<GameError> for MainState {
         if self.enemies.is_empty() {
             let won_text = Text::new("You won!");
             let won_position = [
-                WINDOW_WIDTH / 2.0 - won_text.width(ctx) as f32 / 2.0, 
+                WINDOW_WIDTH / 2.0 - won_text.width(ctx) as f32 / 2.0,
                 WINDOW_HEIGHT / 2.0,
             ];
             graphics::draw(ctx, &won_text, (won_position, 0.0, Color::WHITE))?;
         }
-    
+
         graphics::present(ctx)?;
         Ok(())
     }
-    
+
     fn key_down_event(
         &mut self,
         ctx: &mut Context,
